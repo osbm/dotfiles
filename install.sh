ec490a84d7c255f4747e99f5d05edb3c3a0a0b20
osbm <osmanfbayram@gmail.com>
2023-04-10 16:06:35 +0300

    fix the installer script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 # change default shell to zsh
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
 
 # create my folders
 mkdir -p ~/Documents/GitHub
@@ -11,10 +11,23 @@ mkdir -p ~/Pictures/screenshots
 mkdir -p ~/Documents/tempdir/old_config_files
 
 link_file() {
+  # if that file exists, move it to the temp folder
   if [ -f ~/$1 ]; then
-    mv ~/$1 ~/Documents/tempdir/old_config_files/$1
+    echo "Moving ~/$1 to ~/Documents/temp/old_config_files/$1"
+    # lets create that folder if it doesn't exist
+    # get the folder name of the &1
+    mkdir -p ~/Documents/temp/old_config_files/$(dirname $1)
+    mv ~/$1 ~/Documents/temp/old_config_files/$1
   fi
-  ln -s src/$1 $2
+
+  # if that file is already a symlink, remove it
+  if [ -L ~/$1 ]; then
+    echo "Removing ~/$1"
+    rm ~/$1
+  fi
+
+  echo "Linking ~/$1 to ~/Documents/GitHub/dotfiles/src/$1"
+  ln -s ~/Documents/GitHub/dotfiles/src/$1 $2
 }
 
 link_file .zshrc ~
