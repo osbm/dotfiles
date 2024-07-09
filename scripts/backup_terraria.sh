9a0ab81b7c5c996984205ff331014af2fefb95d3
osbm <osmanfbayram@gmail.com>
2024-07-10 01:20:05 +0300

    remove scripts folder

--- scripts/backup_terraria.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/.local/share
-zip -r Terraria_$(date +%Y-%m-%d_%H-%M ).zip Terraria/
-huggingface-cli upload --repo-type dataset osbm/terraria-backups Terraria_$(date +%Y-%m-%d_%H-%M ).zip Terraria_$(date +%Y-%m-%d_%H-%M ).zip
